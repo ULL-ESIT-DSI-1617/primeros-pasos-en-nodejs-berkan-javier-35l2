79950ee502ad4ebe0e387140e7f59478aaaf1be7
Javier <alu0100819441@ull.edu.es>
2017-02-19 16:20:53 +0000

    Actualiza gulpfile y package.json.
    Ahora es posible construir el libro mediante el uso de la tarea 'build', ejecutando el comando gulp build.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 var gulp = require('gulp');
-var ghPages = require('gulp-gh-pages')
+var shell = require('gulp-shell');
+var ghPages = require('gulp-gh-pages');
+
+gulp.task('build',function(){
+    return gulp.src('')
+        .pipe(shell(['./scripts/generate-gitbook']));
+});
 
 gulp.task('deploy',function(){
     return gulp.src('./txt/_book/**/*')
